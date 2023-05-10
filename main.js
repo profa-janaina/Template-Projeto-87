6d2fda3b00f0fa59479da1fa28d70ccdcc55db25
profa-janaina <111704392+profa-janaina@users.noreply.github.com>
2023-05-10 17:08:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-var canvas = new fabric.Canvas('myCanvas');
+// criar uma variável para o canvas
+
  block_y=1;
  block_x=1;
 
@@ -10,18 +11,7 @@ var block_image_object= "";
 
 function new_image(get_image)
 {
-	fabric.Image.fromURL(get_image, function(Img) {
-	block_image_object = Img;
-		
-	block_image_object.scaleToWidth(block_image_width);
-	block_image_object.scaleToHeight(block_image_height);
-	block_image_object.set({
-	top:block_y,
-	left:block_x
-	});
-	canvas.add(block_image_object);
-
-	});
+	// enviar as imagens
 
 }
 
